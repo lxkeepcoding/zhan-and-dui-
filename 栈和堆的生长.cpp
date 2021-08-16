d249be74b0aef062b9039a42d247b3a4c1f3c643
lxkeepcoding <lx3332551532@163.com>
2021-08-16 23:49:57 +0800

    first

---
+++ 栈和堆的生长.cpp
@@ -0,0 +1,37 @@
+//#include<iostream>
+//using namespace std;
+//
+//void f2()
+//{
+//	int b = 0;
+//	cout <<"b:"<< &b << endl;
+//	cout << endl;
+//	cout << endl;
+//}
+//
+//void f1()
+//{
+//	int a = 0;
+//	cout<<"a:" << &a << endl;
+//	cout << endl;
+//	cout << endl;
+//	f2();
+//}
+//
+//int main()
+//{
+//	f1();
+//	return 0;
+//}
+
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int *p1 = (int *)malloc(4);
+//	int *p2 = (int *)malloc(4);
+//	cout << "p1:" << p1 << endl << endl << endl;
+//	cout << "p2:" << p2 << endl << endl << endl;
+//	return 0;
+//}
